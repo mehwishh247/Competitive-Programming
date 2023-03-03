a393952e65dadbc82c8426713fb1c946ce13a678
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-03 10:14:34 +0300

    Time: 40 ms (63.99%), Space: 13.8 MB (46.90%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        low,high=0,x
-        
+        low,high=0,x  
         while low<=high:
             mid= low + (high-low)//2
             sqr = mid*mid
@@ -12,7 +11,7 @@ class Solution:
                     return mid
                 low=mid+1
             else:
-                if (mid-1)**2<x:
+                if (mid-1)*(mid-1)<x:
                     return mid-1
                 high=mid-1
                 
