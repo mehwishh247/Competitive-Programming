5d2ee59a9d239339511469af48a3d1cf20663649
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-10 17:23:32 +0300

    Time: 50 ms (66.33%), Space: 14.6 MB (10.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        counter=0
         tuples = []
         for i in range(len(nums)):
             tuples.append((nums[i],i))
@@ -14,6 +15,6 @@ class Solution(object):
             else:
                 low+=1
         
-            
+        return counter
         
         
