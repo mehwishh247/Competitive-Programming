3c0f9880e7d3445677a10acebc1b48d2d8711d6b
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-15 12:36:19 +0300

    Time: 26 ms (97.04%), Space: 14 MB (77.83%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,25 +1,71 @@
 class Solution:
-    def revInv(self,s):
-            result = ""
-            for i in range(len(s)):
-                if s[i]=="1":
-                    result+="0"
-                else:
-                    result+="1"
-            return result[::-1]
-    
-    def findKthBit(self, n: int, k: int) -> str:
-        
-        def recur(n):
-            if n==1:
-                return "0"
-            temp=recur(n-1)    
-            ans= temp+"1"+self.revInv(temp)
+        def findKthBit(self, n: int, k: int) -> str:
             
-            return ans
+            def solver(n,k):
+                if n==1:
+                    return "0"
+                
+                size = 2**n-1
+                
+                if k<size//2+1:
+                    return solver(n-1,k)
+                elif k>size//2+1:
+                    backsize = 2**(n-1)-1 
+                    gap = k-(size//2+1)
+                    return str(1-int(solver(n-1,backsize-gap+1)))
+                else:
+                    return '1'
+                    
+            return solver(n, k)
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #     def revInv(self,s):
+#             result = ""
+#             for i in range(len(s)):
+#                 if s[i]=="1":
+#                     result+="0"
+#                 else:
+#                     result+="1"
+#             return result[::-1]
+    
+    # def findKthBit(self, n: int, k: int) -> str:
+        
+#         def recur(n):
+#             if n==1:
+#                 return "0"
+#             temp=recur(n-1)    
+#             ans= temp+"1"+self.revInv(temp)
+            
+#             return ans
        
-        x = recur(n)
-        return x[k-1]
+#         x = recur(n)
+#         return x[k-1]
         
         
       
