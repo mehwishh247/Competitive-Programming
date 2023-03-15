7a43fe865c20b73f05a3e8d29ee173b1c9e9e1c6
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-15 16:39:51 +0300

    Time: 36 ms (42.42%), Space: 13.9 MB (12.33%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -4,26 +4,56 @@
 #         self.val = val
 #         self.next = next
 
-        
+
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = head
-        def solve(prev, node):
-            if not node or not node.next:
-                return node
+        left = dummy = ListNode(0,head)
+
+        while left.next and left.next.next:
+            second = left.next
+            third = second.next
+
+            second.next = third.next
+            third.next = second
+
+            left.next = third
+            left = second
+
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+# class Solution:
+#     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev = head
+#         def solve(prev, node):
+#             if not node or not node.next:
+#                 return node
             
-            node2 = node.next
-            tail = node2.next
-            node2.next = node
-            node.next = solve(node, tail)
+#             node2 = node.next
+#             tail = node2.next
+#             node2.next = node
+#             node.next = solve(node, tail)
             
-            if prev == head:
-                return node2
-            else:
-                prev.next = node2
-                return node2
+#             if prev == head:
+#                 return node2
+#             else:
+#                 prev.next = node2
+#                 return node2
             
-        return solve(prev, head)
+#         return solve(prev, head)
 
     
     
