71f8eff59008e0e808391efe3e793ed94b0b59be
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-16 16:36:12 +0300

    Time: 50 ms (86.47%), Space: 14 MB (16.79%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,11 +3,13 @@ class Solution:
         ans=[]
         candidates.sort()
         def recSol(idx,summ,array):
-            if summ>target:
-                return 
             if summ==target:
                 ans.append(array.copy())
                 return 
+        
+            if idx>=len(candidates) or summ>target or target-summ < candidates[idx]:
+                return 
+
             visited = set()
             for i in range(idx,len(candidates)):
                 if candidates[i] not in visited:
