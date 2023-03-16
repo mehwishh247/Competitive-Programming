d61a379bf5978a33f73668038fd0660972042027
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-16 11:06:39 +0300

    Time: 55 ms (84.39%), Space: 14.1 MB (22.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,7 +2,7 @@ class Solution:
     
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.ans=[]
-        def recSol(candidates,summ,array):
+        def recSol(idx,summ,array):
             if summ>target:
                 return 
             if summ==target:
@@ -10,13 +10,13 @@ class Solution:
                 self.ans.append(array)
                 return
                 
-            for i in range(len(candidates)):
+            for i in range(idx,len(candidates)):
                 summ+=candidates[i]
                 array.append(candidates[i])
-                recSol(candidates[i::],summ,array)
+                recSol(i,summ,array)
                 out = array.pop()
                 summ-=out   
-        recSol(candidates,0,[])
+        recSol(0,0,[])
         
         return self.ans
                 
