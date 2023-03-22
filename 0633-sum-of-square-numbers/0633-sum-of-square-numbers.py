f0125819883956a93f90f8a936790f6de49bcc4e
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-22 15:48:36 +0300

    Time: 346 ms (34.68%), Space: 13.8 MB (50.22%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPerfectSquare(self, num):
+        if num**0.5==int(num**0.5):
+            return True
+        return False
+    
+    def judgeSquareSum(self, c: int) -> bool:
+        # if c==1 or c==0:
+        #     return True
+        limit = int(c**0.5)+1
+        for i in range(0,limit):  
+            if self.isPerfectSquare(c-i**2):
+                return True
+            
+        return False
+            
+                
