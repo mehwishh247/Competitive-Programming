de48dd2159dc11956378099a16a46bd786a77474
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-23 15:31:13 +0300

    Time: 788 ms (93.17%), Space: 49.8 MB (61.13%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -5,8 +5,13 @@ class Solution:
         for n1, n2 in edges:
             graph[n1].append(n2)
             graph[n2].append(n1)
+            if len(graph[n1])>1:
+                return n1
+            if len(graph[n2])>1:
+                return n2
+       
             
-        for i in graph.keys():
-            if len(graph[i])>1:
-                return i
+        # for i in graph.keys():
+        #     if len(graph[i])>1:
+        #         return i
     
