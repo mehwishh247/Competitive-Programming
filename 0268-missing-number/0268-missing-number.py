89040cca1fa4bf951b15b72ff885f74b57ac3ad0
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-23 19:33:28 +0300

    Time: 165 ms (26.64%), Space: 15.1 MB (67.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,18 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n= len(nums)
-        tot = n*(n+1)//2
-        return tot-sum(nums)
+    def missingNumber(self, arr: List[int]) -> int:
+        minn = min(arr)
+        i=0
+        while i<len(arr):
+            if arr[i]-minn==i:
+                i+=1
+            else:
+                temp = arr[i]-minn
+                if temp>=len(arr)-1:
+                    i+=1
+                    continue
+                arr[i],arr[temp] = arr[temp], arr[i]
+        
+        for i in range(len(arr)):
+            if i!=arr[i]:
+                return i
+        return i+1
