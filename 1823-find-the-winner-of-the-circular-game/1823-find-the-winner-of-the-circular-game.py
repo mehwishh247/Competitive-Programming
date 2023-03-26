935075306ca2b1055e511c513b8e28d0eb1dc8bd
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-03-26 16:48:59 +0300

    Time: 2223 ms (5.05%), Space: 13.9 MB (72.62%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+    
+
+            arr = [x for x in range(1,n+1)]
+            cap = n
+            c=k-1
+            p=0
+            while cap>1:
+                if p==c and arr[p]!=0:
+                    arr[p]=0
+                    c=(c+k)%n
+                    # p=p+1
+                    cap-=1 
+                p=(p+1)%n
+                if arr[p]==0:
+                    c= (c+1)%n
+
+            arr.sort()
+            return arr[-1]
