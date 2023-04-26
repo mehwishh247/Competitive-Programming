6df1196fcbc6c0cd4a0f64c976dd514c1c58471c
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-04-26 19:28:15 +0300

    Time: 55 ms (49.68%), Space: 14 MB (35.38%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,42 @@
+class Solution:
+    
+    def isValid(self,dr,board):
+        return (0<=dr[0]<len(board)) and (0<=dr[1]<len(board[0])) and (board[dr[0]][dr[1]]==1)
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+    
+        visited = set()
+        queue = deque()
+        
+        # find all rottens and add them to the que and visisted
+        
+        rowL = len(grid)
+        colL = len(grid[0])
+        
+        for row in range(rowL):
+            for col in range(colL):
+                if grid[row][col]==2:
+                    visited.add((row,col,0))
+                    queue.append((row,col,0))
+
+        level=0
+
+        while queue:
+            nodeData = queue.popleft()
+            rrow = nodeData[0]
+            ccol = nodeData[1]
+            level = nodeData[2]
+            
+            directions = [(rrow+1,ccol),(rrow-1,ccol),(rrow,ccol+1),(rrow,ccol-1)]
+            for dr in directions:
+                if self.isValid(dr,grid):
+                    grid[dr[0]][dr[1]]=2
+                    queue.append((dr[0],dr[1],level+1))
+                    
+        for row in range(rowL):
+            for col in range(colL):
+                if grid[row][col]==1:
+                    return -1
+        
+        
+
+        return level
