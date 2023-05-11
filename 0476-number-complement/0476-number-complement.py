801edf49980d13a6ad13136978dbc67a329c525d
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-05-11 10:44:54 +0300

    Time: 41 ms (17.29%), Space: 16.3 MB (8.53%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,9 +1,14 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         
-            x = num
-            mask =1
-            while mask<=x:
-                x = x ^ mask
-                mask= mask << 1
-            return x
+            # x = num
+            # mask =1
+            # while mask<=x:
+            #     x = x ^ mask
+            #     mask= mask << 1
+            # return x
+            mask_len=len(bin(num))-2
+            mask='1' * mask_len
+            mask_n=int(mask,2)
+            return num ^ mask_n
+        
