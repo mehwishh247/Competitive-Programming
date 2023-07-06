7fa1c808281bb961e08d420beb1bb6d706e1cbd4
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-07-06 20:15:03 +0300

    Time: 478 ms (87.77%), Space: 16.3 MB (71.81%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.py
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.py
@@ -1,14 +1,15 @@
 class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
         
-        loopNum = total//max(cost1,cost2)
+        a = max(cost1,cost2)
+        b = min(cost1,cost2)
         
+        loopNum = total//a
         ans = 0
         
         for i in range(loopNum+1):
             
-            maxRange = (total-max(cost1,cost2)*i)//min(cost1,cost2)
-            
+            maxRange = (total-a*i)//b
             ans += maxRange+1
             
         return ans
