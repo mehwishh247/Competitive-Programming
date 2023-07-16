ec1ed59e85e459f0f4f4d200830e2a6903a1954e
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-07-16 17:36:30 +0300

    Time: 102 ms (38.53%), Space: 19.8 MB (50.45%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,15 +1,4 @@
 class Solution:
-    
-    def findPosition(self,intervals,target):
-        low,high=0,len(intervals)
-        
-        while low < high:
-            mid=low + (high-low)//2
-            if intervals[mid][0]<target:        
-                low=mid+1
-            else:
-                high=mid
-        return low
             
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         '''
@@ -39,60 +28,4 @@ class Solution:
 
         return merged
 
-#         indexS = self.findPosition(intervals,newInterval[0])
-#         indexE = self.findPosition(intervals,newInterval[-1])
-
-
-#         print(indexS,indexE)
-
-#         n = len(intervals)     
-        
-        
-#         if indexS>=1:
-#             a,b = intervals[index-1]
-#             startT,endT = newInterval 
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if index>=1 and index<=n-1:
-#             # check if its start is in its behind interval
-#             a,b = intervals[index-1]
-#             c,d = intervals[index] 
-#             startT,endT = newInterval 
-            
-#             print(c,d,startT,endT)
-#             if a <= startT <= b:
-                
-#                 if endT>b:
-#                     intervals[index-1][-1] = endT
-#                     return intervals
-
-#             elif c <= startT <= d:
-
-#                 if endT>d:
-#                     intervals[index][-1] = endT
-#                     return intervals
-
-
-                
-
-            
-                
-                
-            
-            
-        
-        
-        return intervals
-        
-        
-            
+#
