ef42fa0a59e787594921cb74327970644b730961
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-07-25 00:05:23 +0300

    Time: 78 ms (36.35%), Space: 16.6 MB (24.38%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -1,23 +1,15 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
-        def quickSelect(arr, start, end, k):
-            pivot = arr[end]
-            left = start
-
-            for right in range(start, end):
-                if arr[right] <= pivot:
-                    arr[right], arr[left] = arr[left], arr[right]
-                    left += 1
-
-            arr[left], arr[end] = arr[end], arr[left]
-
-            if left > k: return quickSelect(arr, start, left-1, k)
-            elif left < k: return quickSelect(arr, left + 1, end, k)
-            else: return arr[left]
-
+        
         n = len(arr)
-        k = n // 20
-        quickSelect(arr, 0, len(arr) - 1, k)
-        quickSelect(arr, k, n-1, n-k)
-
-        return mean(arr[k:-k])
+        arr.sort()
+        
+        start =  int(n*0.05)
+        end = int(n*0.95)
+        
+        total = 0
+        
+        for i in range(start,end,1):
+            total+=arr[i]
+            
+        return total/(end-start)
