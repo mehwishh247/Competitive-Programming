4a00842148054afbcc2ff3e630a2a8ba276e6745
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-07-28 21:08:48 +0300

    Time: 222 ms (100.00%), Space: 19.4 MB (73.67%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -1,10 +1,14 @@
 class Solution:
     def maxSum(self, grid: List[List[int]]) -> int:
-        currentMax=0
-        for row in range(len(grid)-2):
-            for col in range(len(grid[0])-2):
-                total=sum(grid[row][col:col+3])+sum(grid[row+2][col:col+3])+grid[row+1][col+1]
-                # print(total)
-                currentMax=max(total,currentMax)
-        return currentMax
-    
+        
+        m,n = len(grid[0]),len(grid)
+        currMax = float("-inf")
+
+        for row in range(n-2):
+            for col in range(m-2):
+                
+                top = grid[row][col] + grid[row][col+1]+grid[row][col+2]
+                btm = grid[row+2][col] + grid[row+2][col+1]+grid[row+2][col+2]
+                currMax = max(currMax,top+btm+grid[row+1][col+1])
+        
+        return currMax
