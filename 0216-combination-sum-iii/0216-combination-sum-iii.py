087e2cbe3140abff8725ee0af864888ac3d9e260
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-10-03 16:37:59 +0300

    Time: 43 ms (29.75%), Space: 16.4 MB (9.58%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -2,26 +2,28 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ans=[]
         candidates = [x for x in range(1,10)]
+        self.sum = 0
         target = n
-        def recSol(idx,summ,array):
-            if summ==target and len(array)==k:
+        
+        def recSol(idx,array):
+            if self.sum==target and len(array)==k:
                 ans.append(array.copy())
                 return 
         
-            if idx>=len(candidates) or summ>target or target-summ < candidates[idx]:
+            if idx>=len(candidates) or self.sum>target or target-self.sum < candidates[idx]:
                 return 
 
             visited = set()
             for i in range(idx,len(candidates)):
                 if candidates[i] not in visited:
                     visited.add(candidates[i])
-                    summ+=candidates[i]
+                    self.sum+=candidates[i]
                     array.append(candidates[i])
-                    recSol(i+1,summ,array)
+                    recSol(i+1,array)
                     out = array.pop()
-                    summ-=out 
+                    self.sum-=out 
                     
-        recSol(0,0,[])
+        recSol(0,[])
         return ans
                 
                
