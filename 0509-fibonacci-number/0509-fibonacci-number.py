5be5288fa6f305778222cb08c5b78b84a9193553
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-10-09 18:00:18 +0300

    Time: 32 ms (92.66%), Space: 16.2 MB (79.00%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        
         if n==0:
             return 0
-        
-        arr= [0,1]
+        arr= deque([0,1])
         
         for i in range(1,n):
             arr.append(arr[-1]+arr[-2])
-
-            
+            arr.popleft()
+        
         return arr[-1]
         
