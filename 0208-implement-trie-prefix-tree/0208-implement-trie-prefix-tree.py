24b7ef54c5e0bafc40b0c62fe8c80bf7636aa05c
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-10-10 16:21:32 +0300

    Time: 203 ms (16.42%), Space: 35.4 MB (15.37%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -6,38 +6,44 @@ class Trie:
 
     def insert(self, word: str) -> None:
         curr = self.root
-        
-        for c in word:
-            curr = curr.kids[c]
+        for i,ltr in enumerate(word):
+            idx = ord(ltr)-97
+            if not curr.have[idx]:
+                curr.have[idx] = TrieNode()
+            curr = curr.have[idx]
+            
         curr.is_end = True
             
 
     def search(self, word: str) -> bool:
         curr = self.root
-        
-        for c in word:           
-            if c not in curr.kids:
-                return False
-            curr = curr.kids[c]
+        for i,ltr in enumerate(word):
+            idx = ord(ltr)-97
+            if not curr.have[idx]: return False
+            
+            curr = curr.have[idx]
         return curr.is_end
+            
             
         
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.root
-        
-        for c in prefix:           
-            if c not in curr.kids:
-                return False
-            curr = curr.kids[c]
+        for i,ltr in enumerate(prefix):
+            idx = ord(ltr)-97
+            if not curr.have[idx]: return False
+            
+            curr = curr.have[idx]
         return True
+            
         
 
         
 class TrieNode:
     def __init__(self):
-        self.is_end  = False
-        self.kids = defaultdict(lambda: TrieNode())
+        self.have = [None]*26
+        self.is_end = False
+    
 
 
 # Your Trie object will be instantiated and called as such:
