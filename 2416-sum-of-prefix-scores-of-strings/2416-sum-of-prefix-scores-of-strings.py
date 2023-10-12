ca98ab4b62d2f90f62016c22a0a34249880e730a
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-10-12 14:47:31 +0300

    Time: 5455 ms (14.66%), Space: 447.1 MB (35.82%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,54 @@
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        
+        tri = Trie()
+        
+        for word in words:
+            tri.insert(word)
+        result = []
+        for word in words:
+            result.append(tri.score(word))
+        return result
+        
+
+        
+class TrieNode:
+    def __init__(self):
+        self.have = [None]*26
+        self.pc = 0
+        self.wc = 0
+        
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self,word):
+        
+        curr = self.root
+        for i,ltr in enumerate(word):
+            
+            idx = ord(ltr)-97
+            
+            if not curr.have[idx]: 
+                curr.have[idx] = TrieNode()
+            
+            curr.have[idx].pc+=1
+            curr = curr.have[idx]
+            
+        curr.wc +=1
+        
+    def score(self,word):
+        
+        summ = 0
+        curr = self.root
+        
+        for i,ltr in enumerate(word):
+            
+            idx = ord(ltr)-97
+            
+            summ+= curr.have[idx].pc
+            
+            curr = curr.have[idx]
+        return summ
+            
