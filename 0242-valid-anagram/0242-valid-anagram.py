124145a0b6afbe8442d21402b32c1b51142e3be9
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-11-25 10:35:48 +0300

    Time: 65 ms (10.98%), Space: 17.6 MB (7.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        # t --> s = True else False
+        # can we rearrage s and use it's elements once then get t
         
-        if sorted(s)==sorted(t):
-            return True
+        if len(s) != len(t): return False
         
-        return False
+        sorted_t = sorted(t)
+        sorted_s = sorted(s)
+
+        for i in range(len(t)):
+            
+            if sorted_t[i]!=sorted_s[i]:
+                return False
+        return True
+            
+        
+        
