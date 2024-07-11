2b3e4242a34e373f7a2bca2d4553b55019ea721d
Mehwish Mumtaz <39596277+mehwishh247@users.noreply.github.com>
2024-07-11 11:20:35 +0500

    Update 0001-two-sum.py
    Fork to suggest changes to the code to make it more optimized.
    The code is shorter, cleaner, and uses pre-built libraries which is a good way to demonstrate knowledge of multiple modules. pre-built module is also optimized for performance so, it will make the code quicker than it is before.

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,10 @@
+from itertools import combinations
+
 class Solution(object):
+
     def twoSum(self, nums, target):
-        # counter=0
-        tuples = []
-        for i in range(len(nums)):
-            tuples.append((nums[i],i))
-        tuples.sort()
-        low,hi=0,len(nums)-1
-        while low<hi:
-            tot = tuples[low][0]+tuples[hi][0] 
-            if tot == target:
-                return [tuples[low][1],tuples[hi][1]]
-            if tot>target:
-                hi-=1
-            else:
-                low+=1
+        pairs = combinations(nums, 2)
         
-        # return counter
-        
-        
+        for i in pairs:
+            if sum(i) == target:
+                return i
